e96c4820a79117018f95c0c22de37e2953e8dab6
Montant Andréa <andrea.montant@epfl.ch>
2018-11-05 19:25:16 +0100

    final2

--- src/network.cpp
+++ src/network.cpp
@@ -22,8 +22,7 @@ void Network::resize(const size_t& n){ /// A REVOIR!! -> RESIZE??
   @param[in] a,b the indexes if the two nodes 
   @param[out] success the link was succesfully inserted
  */	
-bool Network::add_link(const size_t& a, const size_t& b){ //a est lié avec b, l'inverse aussi et retourne si ça a marché
-	//Si a-b et b-a n'existe pas déjà les créer ie rajouter comme mapped value de values[a] b et inversement
+bool Network::add_link(const size_t& a, const size_t& b){ 
 
 		bool is_linked_with; 
 	for(size_t i=0; i<neighbors(a).size();++i){
@@ -50,29 +49,22 @@ bool Network::add_link(const size_t& a, const size_t& b){ //a est lié avec b, l
   @param mean_deg the average of the Poisson distribution.
  */
  
- /*effacer tous les liens relié à n
-  * générer un nombre aléatoire qui sera le nombre de lien avec n
-  * Mais ces liens doivent être relié à un noeud cible, lequel?
-  * */
-size_t Network::random_connect(const double& n){ ///!!!!! A REVOIR
+size_t Network::random_connect(const double& n){
 	links.clear();//  All previous links are cleared first.
 	RandomNumbers rng; //déclaration de la class 
 	std::vector<int> random_index;
-	size_t poisson;
-	poisson=rng.poisson(n); //génère un nombre aléatoire
-	random_index.resize(poisson); //adapte la taille du tableau
-	size_t res (0); //nombre de lien au total
+	random_index.resize(rng.poisson(n)); //adapte la taille du tableau
+	size_t nb_links (0); //nombre de lien au total
 	for (size_t i=0; i<values.size(); ++i){ //on veut attribuer à chacun des nouveaux nodes une valeur grâce à une répartition uniforme
 		rng.uniform_int(random_index, 0, size()); //rempli random_index de valeur de distribution uniforme
 		for(size_t j=0; j< random_index.size();++j){
 			if (add_link(i, random_index[j])){ //si on peut faire un lien avec un des noeuds de random_index 
-				//PAS CONTENT -bug
-			res++;} //return le nombre de lien au total
+			nb_links++;} //return le nombre de lien au total
 			}
 
 		}
 
-	return res; 
+	return nb_links; 
 
 	
 	}
@@ -86,7 +78,7 @@ size_t Network::set_values(const std::vector<double>& tab){
 	values.clear();
 	
 	resize (tab.size());
-	values= tab; //copie de tab dans values
+	values= tab; 
 	return tab.size();
 	
 	}
@@ -135,10 +127,4 @@ std::vector<size_t> Network::neighbors(const size_t& n) const{
   return (neigh_val);
 
 	}
-	
-	
-	/*
-	 * Find crée un itérateur de type auto qui va se placé sur la premiere occurence de la clef puis il faut l'implementer avec la condition d'arrêt adéquate
-	 * Count compte le nombre d'occurence de la clef souhaitée
-	 * neigbour renvoie les valeurs des noeuds associé à la clef en question*/
 	
